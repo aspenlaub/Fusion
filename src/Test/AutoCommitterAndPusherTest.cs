3fc9e9a8ca5896f97641e75ec7e35d098d2b2c55
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-02-11 21:31:36 +0100

    Remove temporary test method

--- src/Test/AutoCommitterAndPusherTest.cs
+++ src/Test/AutoCommitterAndPusherTest.cs
@@ -1,7 +1,5 @@
-﻿using System.Threading.Tasks;
-using Aspenlaub.Net.GitHub.CSharp.Fusion.Interfaces;
+﻿using Aspenlaub.Net.GitHub.CSharp.Fusion.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.Gitty.TestUtilities;
-using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
 using Autofac;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,13 +11,6 @@ namespace Aspenlaub.Net.GitHub.CSharp.Fusion.Test {
         public void CanConstructAutoCommitterAndPusher() {
             var container = new ContainerBuilder().UseGittyTestUtilities().UseFusionNuclideProtchAndGitty().Build();
             Assert.IsNotNull(container.Resolve<IAutoCommitterAndPusher>());
-        }
-
-        [TestMethod]
-        public async Task CanAutoCommitAndPushCake() {
-            var container = new ContainerBuilder().UseGittyTestUtilities().UseFusionNuclideProtchAndGitty().Build();
-            var autoCommitterAndPusher = container.Resolve<IAutoCommitterAndPusher>();
-            await autoCommitterAndPusher.AutoCommitAndPushSingleCakeFileAsync(new Folder(@"D:\Users\Wolfgang\GitHub\ChabStandard")); // ToDo: remove this if it works
         }
     }
 }
