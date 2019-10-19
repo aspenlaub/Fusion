4ff3fd5107e0f782ff2b946e8080d3042ac4f1b6
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-10-19 16:43:07 +0200

    Nuclide update

--- src/Test/NugetPackageUpdaterTest.cs
+++ src/Test/NugetPackageUpdaterTest.cs
@@ -19,7 +19,7 @@ namespace Aspenlaub.Net.GitHub.CSharp.Fusion.Test {
     [TestClass]
     public class NugetPackageUpdaterTest {
         private static readonly TestTargetFolder PakledConsumerCoreTarget = new TestTargetFolder(nameof(NugetPackageUpdaterTest), "PakledConsumerCore");
-        private const string PakledConsumerCoreHeadTipSha = "e2012ba3bfbaff0ab985cd2629b1a5b368410ace"; // Before PakledCoreUpdate
+        private const string PakledConsumerCoreHeadTipSha = "5a221d91dc13e66f09327cd61c42d85036553d14"; // Before PakledCoreUpdate
         private static IContainer vContainer;
         private static TestTargetInstaller vTargetInstaller;
 
@@ -55,7 +55,7 @@ namespace Aspenlaub.Net.GitHub.CSharp.Fusion.Test {
             Assert.IsFalse(errorsAndInfos.Errors.Any(), errorsAndInfos.ErrorsPlusRelevantInfos());
             var yesNo = await NugetUpdateOpportunitiesAsync(errorsAndInfos);
             Assert.IsTrue(yesNo);
-            Assert.IsTrue(errorsAndInfos.Infos.Any(i => i.Contains("package Aspenlaub.Net.GitHub.CSharp.PakledCore from 1.0.6974.31928")));
+            Assert.IsTrue(errorsAndInfos.Infos.Any(i => i.Contains("package Aspenlaub.Net.GitHub.CSharp.PakledCore from 1.0.7231.23351")));
         }
 
         [TestMethod]
