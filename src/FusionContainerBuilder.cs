0e5da9663e01a486df3585864068eba92316ed7e
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-10-24 21:28:34 +0200

    Nuclide update

--- src/FusionContainerBuilder.cs
+++ src/FusionContainerBuilder.cs
@@ -2,17 +2,23 @@
 using Autofac;
 using Microsoft.Extensions.DependencyInjection;
 using Aspenlaub.Net.GitHub.CSharp.Nuclide;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
 using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
+// ReSharper disable UnusedMember.Global
 
 namespace Aspenlaub.Net.GitHub.CSharp.Fusion {
     public static class FusionContainerBuilder {
+        public static IContainer CreateContainerUsingFusionNuclideProtchAndGitty() {
+            return new ContainerBuilder().UseFusionNuclideProtchAndGitty(new DummyCsArgumentPrompter()).Build();
+        }
+
         public static ContainerBuilder UseFusionNuclideProtchAndGitty(this ContainerBuilder builder, ICsArgumentPrompter csArgumentPrompter) {
             builder.UseNuclideProtchGittyAndPegh(csArgumentPrompter);
             builder.RegisterType<NugetPackageUpdater>().As<INugetPackageUpdater>();
             builder.RegisterType<AutoCommitterAndPusher>().As<IAutoCommitterAndPusher>();
             return builder;
         }
-        // ReSharper disable once UnusedMember.Global
+
         public static IServiceCollection UseFusionNuclideProtchAndGitty(this IServiceCollection services, ICsArgumentPrompter csArgumentPrompter) {
             services.UseNuclideProtchGittyAndPegh(csArgumentPrompter);
             services.AddTransient<INugetPackageUpdater, NugetPackageUpdater>();
