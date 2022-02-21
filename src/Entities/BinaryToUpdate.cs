7043060469665d271f5cf68af773c48cffc04d33
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2022-02-21 20:33:10 +0100

    Update to .Net 6

--- src/Entities/BinaryToUpdate.cs
+++ src/Entities/BinaryToUpdate.cs
@@ -2,5 +2,9 @@
     public class BinaryToUpdate {
         public string FileName { get; set; }
         public string UpdateReason { get; set; }
+
+        public override string ToString() {
+            return $"{FileName} ({UpdateReason})";
+        }
     }
 }
--- src/Test/ChangedBinariesListerTest.cs
+++ src/Test/ChangedBinariesListerTest.cs
@@ -9,9 +9,11 @@ using Autofac;
 namespace Aspenlaub.Net.GitHub.CSharp.Fusion.Test {
     [TestClass]
     public class ChangedBinariesListerTest {
-        private const string BeforeMajorChangeHeadTipSha = "932cb235841ce7ab5afc80fcbc3220c4ae54933e";
-        private const string PreviousHeadTipIdSha = "6e314114c347c17776bdd8367cc5d0f1687a7775";
-        private const string CurrentHeadTipIdSha = "b09bf637ae6eb84e098c81da6281034ea685f307";
+        private const string BeforeMajorPeghChangeHeadTipSha = "932cb235841ce7ab5afc80fcbc3220c4ae54933e";
+        private const string PreviousPeghHeadTipIdSha = "6e314114c347c17776bdd8367cc5d0f1687a7775";
+        private const string CurrentPeghHeadTipIdSha = "b09bf637ae6eb84e098c81da6281034ea685f307";
+        private const string CurrentProuserHeadTipIdSha = "1719eb27a77904c88886a184ddfbcfeb4419000d";
+        private const string PreviousProuserHeadTipIdSha = "7418e247ddee3caf5b841187df868a1ecbaab7bc";
 
         private readonly IContainer Container;
 
@@ -20,26 +22,45 @@ namespace Aspenlaub.Net.GitHub.CSharp.Fusion.Test {
         }
 
         [TestMethod]
-        public void UnchangedBinariesAreNotListed() {
+        public void UnchangedPeghBinariesAreNotListed() {
             var sut = Container.Resolve<IChangedBinariesLister>();
             Assert.IsNotNull(sut);
             var errorsAndInfos = new ErrorsAndInfos();
-            var changedBinaries = sut.ListChangedBinaries("Pegh", PreviousHeadTipIdSha, CurrentHeadTipIdSha, errorsAndInfos);
+            var changedBinaries = sut.ListChangedBinaries("Pegh", PreviousPeghHeadTipIdSha, CurrentPeghHeadTipIdSha, errorsAndInfos);
             Assert.IsFalse(errorsAndInfos.AnyErrors(), errorsAndInfos.ErrorsPlusRelevantInfos());
             Assert.IsFalse(changedBinaries.Any());
         }
 
         [TestMethod]
-        public void CanListChangedBinaries() {
+        public void CanListChangedPeghBinaries() {
             var sut = Container.Resolve<IChangedBinariesLister>();
             Assert.IsNotNull(sut);
             var errorsAndInfos = new ErrorsAndInfos();
-            var changedBinaries = sut.ListChangedBinaries("Pegh", BeforeMajorChangeHeadTipSha, CurrentHeadTipIdSha, errorsAndInfos);
+            var changedBinaries = sut.ListChangedBinaries("Pegh", BeforeMajorPeghChangeHeadTipSha, CurrentPeghHeadTipIdSha, errorsAndInfos);
             Assert.IsFalse(errorsAndInfos.AnyErrors(), errorsAndInfos.ErrorsPlusRelevantInfos());
             Assert.AreEqual(3, changedBinaries.Count);
             Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Aspenlaub.Net.GitHub.CSharp.Pegh.dll"));
             Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Aspenlaub.Net.GitHub.CSharp.Pegh.pdb"));
             Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Aspenlaub.Net.GitHub.CSharp.Pegh.deps.json"));
+        }
+
+        [TestMethod]
+        public void CanListChangedProuserBinaries() {
+            var sut = Container.Resolve<IChangedBinariesLister>();
+            Assert.IsNotNull(sut);
+            var errorsAndInfos = new ErrorsAndInfos();
+            var changedBinaries = sut.ListChangedBinaries("Prouser", PreviousProuserHeadTipIdSha, CurrentProuserHeadTipIdSha, errorsAndInfos);
+            Assert.IsFalse(errorsAndInfos.AnyErrors(), errorsAndInfos.ErrorsPlusRelevantInfos());
+            Assert.AreEqual(9, changedBinaries.Count);
+            Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Aspenlaub.Net.GitHub.CSharp.Prouser.deps.json"));
+            Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Aspenlaub.Net.GitHub.CSharp.Prouser.dll"));
+            Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Aspenlaub.Net.GitHub.CSharp.Prouser.pdb"));
+            Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Microsoft.Web.WebView2.Core.dll"));
+            Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Microsoft.Web.WebView2.WinForms.dll"));
+            Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Microsoft.Web.WebView2.Wpf.dll"));
+            Assert.IsTrue(changedBinaries.Any(c => c.FileName == @"runtimes\win-arm64\native\WebView2Loader.dll"));
+            Assert.IsTrue(changedBinaries.Any(c => c.FileName == @"runtimes\win-x64\native\WebView2Loader.dll"));
+            Assert.IsTrue(changedBinaries.Any(c => c.FileName == @"runtimes\win-x86\native\WebView2Loader.dll"));
         }
     }
 }
