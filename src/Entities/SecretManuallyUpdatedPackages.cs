745cda72f48066545dfe0e4459dd64ab74833b6f
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2023-08-14 16:35:29 +0200

    Cosmetics

--- src/Entities/SecretManuallyUpdatedPackages.cs
+++ src/Entities/SecretManuallyUpdatedPackages.cs
@@ -6,5 +6,5 @@ public class SecretManuallyUpdatedPackages : ISecret<ManuallyUpdatedPackages> {
     private ManuallyUpdatedPackages _DefaultManuallyUpdatedPackages;
     public ManuallyUpdatedPackages DefaultValue => _DefaultManuallyUpdatedPackages ??= new ManuallyUpdatedPackages { new() { Id = "LibGit2Sharp" } };
 
-    public string Guid => "{7185ADA1-2632-4B70-A76E-6CAB7B660DDD}";
+    public string Guid => "7185ADA1-2632-4B70-A76E-6CAB7B660DDD";
 }
