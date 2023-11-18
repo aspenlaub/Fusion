0366bcdbad3cf3b98c3c93866a3958e93a23e635
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2023-11-18 13:53:52 +0100

    build.cake must not be deleted if it's linked in a project file

--- src/Test/ChangedBinariesListerTest.cs
+++ src/Test/ChangedBinariesListerTest.cs
@@ -14,6 +14,9 @@ public class ChangedBinariesListerTest {
     private const string PreviousPeghHeadTipIdSha = "6e314114c347c17776bdd8367cc5d0f1687a7775";
     private const string CurrentPeghHeadTipIdSha = "b09bf637ae6eb84e098c81da6281034ea685f307";
 
+    private const string PreviousShatilayaHeadTipIdSha = "e8f2ae2cf737e1886ab4a0da30cd5dc0a4509ca1";
+    private const string CurrentShatilayaHeadTipIdSha = "bdba763bf1a70defd567b4b5b7b75767c2c23873";
+
     private readonly IContainer _Container
         = new ContainerBuilder().UseFusionNuclideProtchAndGitty("Fusion", new DummyCsArgumentPrompter()).Build();
 
@@ -38,5 +41,16 @@ public class ChangedBinariesListerTest {
         Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Aspenlaub.Net.GitHub.CSharp.Pegh.dll"));
         Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Aspenlaub.Net.GitHub.CSharp.Pegh.pdb"));
         Assert.IsTrue(changedBinaries.Any(c => c.FileName == "Aspenlaub.Net.GitHub.CSharp.Pegh.deps.json"));
+    }
+
+    [TestMethod]
+    public void CanHandleLinkedBuildCake() {
+        var sut = _Container.Resolve<IChangedBinariesLister>();
+        Assert.IsNotNull(sut);
+        var errorsAndInfos = new ErrorsAndInfos();
+        var changedBinaries = sut.ListChangedBinaries("Shatilaya", "master", PreviousShatilayaHeadTipIdSha,
+            CurrentShatilayaHeadTipIdSha, errorsAndInfos);
+        Assert.IsFalse(errorsAndInfos.AnyErrors(), errorsAndInfos.ErrorsPlusRelevantInfos());
+        Assert.IsFalse(changedBinaries.Any());
     }
 }
