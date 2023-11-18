185567b9e5dca115319d7b707feac29c4f2f42c6
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2023-11-18 15:16:38 +0100

    build.cake can also just be included (not linked) in a project file

--- src/Test/ChangedBinariesListerTest.cs
+++ src/Test/ChangedBinariesListerTest.cs
@@ -17,6 +17,9 @@ public class ChangedBinariesListerTest {
     private const string PreviousShatilayaHeadTipIdSha = "e8f2ae2cf737e1886ab4a0da30cd5dc0a4509ca1";
     private const string CurrentShatilayaHeadTipIdSha = "bdba763bf1a70defd567b4b5b7b75767c2c23873";
 
+    private const string PreviousNuclideHeadTipIdSha = "6c0f886a0eda3bc55a00849c64164d5dae3acd52";
+    private const string CurrentNuclideHeadTipIdSha = "a9bec6ccf1f929e9a44f51197c40fdf5fd19eaa1";
+
     private readonly IContainer _Container
         = new ContainerBuilder().UseFusionNuclideProtchAndGitty("Fusion", new DummyCsArgumentPrompter()).Build();
 
@@ -50,6 +53,11 @@ public class ChangedBinariesListerTest {
         var errorsAndInfos = new ErrorsAndInfos();
         var changedBinaries = sut.ListChangedBinaries("Shatilaya", "master", PreviousShatilayaHeadTipIdSha,
             CurrentShatilayaHeadTipIdSha, errorsAndInfos);
+        Assert.IsFalse(errorsAndInfos.AnyErrors(), errorsAndInfos.ErrorsPlusRelevantInfos());
+        Assert.IsFalse(changedBinaries.Any());
+
+        changedBinaries = sut.ListChangedBinaries("Nuclide", "master", PreviousNuclideHeadTipIdSha,
+            CurrentNuclideHeadTipIdSha, errorsAndInfos);
         Assert.IsFalse(errorsAndInfos.AnyErrors(), errorsAndInfos.ErrorsPlusRelevantInfos());
         Assert.IsFalse(changedBinaries.Any());
     }
